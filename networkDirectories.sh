df84fdd3b63182bf6447b723a2e15cb0216cb746
Aleksandr Priemkin <93442911+baggarot@users.noreply.github.com>
2023-10-09 13:22:35 +0700

    Update networkDirectories.sh

--- networkDirectories.sh
+++ networkDirectories.sh
@@ -13,17 +13,17 @@ chmod 711 /home/$var/.smbcredentials
 [ ! -d "/media/catalog3" ] && mkdir -m 777 /media/catalog3
 chown -R $var /media/ksvn /media/gsnsrv /media/dsl
 
-if ! grep -q '//share.domain/share' "/etc/fstab"; then
+if ! grep -qwF '//share.domain/share' "/etc/fstab"; then
 	echo //share.domain/share	/media/catalog1	cifs	x-systemd.automount,credentials=/home/NSO.LOC/$var/.smbcredentials,rw,$i,iocharset=utf8,file_mode=0777,dir_mode=0777,nofail,_netdev	0	0 >> /etc/fstab
 	echo Директория catalog1 успешно добавлена
 fi
 
-if ! grep -q '//user2.domain/share' "/etc/fstab"; then
+if ! grep -qwF '//user2.domain/share' "/etc/fstab"; then
 	echo //user2.domain/share	/media/catalog2	cifs	x-systemd.automount,credentials=/home/NSO.LOC/$var/.smbcredentials,rw,$i,iocharset=utf8,file_mode=0777,dir_mode=0777,nofail,_netdev	0	0 >> /etc/fstab
 	echo Директория catalog2 успешно добавлена
 fi
 
-if ! grep -q '//user3.domain/share' "/etc/fstab"; then
+if ! grep -qwF '//user3.domain/share' "/etc/fstab"; then
 	echo //user3.domain/share	/media/catalog3	cifs	x-systemd.automount,credentials=/home/NSO.LOC/$var/.smbcredentials,rw,$i,iocharset=utf8,file_mode=0777,dir_mode=0777,nofail,_netdev	0	0 >> /etc/fstab
 	echo Директория catalog3 успешно добавлена
 fi
